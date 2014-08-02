8354b485f61b10bcec69f71583d3336e9a0804a7
liuzhe <liuzhe0223@gmail.com>
2014-08-02 20:15:07 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+turblinks port to Revel
---
+++ turbolinks.go
@@ -0,0 +1,17 @@
+package turbolinks
+
+import (
+	"github.com/golib/revel"
+)
+
+type Turbolinks struct {
+	*revel.Controller
+}
+
+func (c *Turbolinks) Before() {
+
+}
+
+func (c *Turbolinks) After() {
+
+}
